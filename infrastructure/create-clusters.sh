17f90347889003b653a79dce0829bfd18a4c2844
Leon Jalfon <leonjalfon1@gmail.com>
2020-12-07 01:24:10 +0200

    create cluster in sdp-mesh network

--- infrastructure/create-clusters.sh
+++ infrastructure/create-clusters.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gcloud compute instance-groups managed list-instances sdp-mesh --zone=europe-west2-c | grep sdp-mesh- | awk '{print $1}' | while read line ; do gcloud container clusters create $line --zone europe-west2-c --enable-autoscaling --machine-type n1-standard-2 --min-nodes 3 --max-nodes 5 --async --quiet --release-channel regular ; done
+gcloud compute instance-groups managed list-instances sdp-mesh --zone=europe-west2-c | grep sdp-mesh- | awk '{print $1}' | while read line ; do gcloud container clusters create $line --zone europe-west2-c --network sdp-mesh --subnetwork sdp-mesh --enable-autoscaling --machine-type n1-standard-2 --min-nodes 3 --max-nodes 5 --async --quiet --release-channel regular ; done
